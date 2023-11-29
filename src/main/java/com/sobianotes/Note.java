808c79c401429e1e6836908b5d2a4c34ce7637a7
Sobia <sobiacodes@gmail.com>
2023-11-29 22:39:52 +0000

    'Before' annotation added to create a Note object and configured maven properly

---
+++ src/main/java/com/sobianotes/Note.java
@@ -0,0 +1,4 @@
+package com.sobianotes;
+
+public class Note {
+}
---
+++ src/test/java/com/sobianotes/NoteTest.java
@@ -0,0 +1,24 @@
+package com.sobianotes;
+
+// importing my barebones classes for now
+
+
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NoteTest {
+
+    Note note;
+
+    @Before
+    public void before() {
+        Note note = new Note();
+    }
+
+    
+
+}
