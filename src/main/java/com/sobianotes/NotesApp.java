34e1054b1094b1337a861dba921b88290bdaf757
Sobia <sobiacodes@gmail.com>
2023-11-30 00:54:09 +0000

    some GUI related stuff set up, going to pause this and return to writing tests

--- src/main/java/com/sobianotes/NotesApp.java
+++ src/main/java/com/sobianotes/NotesApp.java
@@ -1,13 +1,42 @@
 package com.sobianotes;
 
+import database.DatabaseManager;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+// SQL related stuff
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
 /**
  * Hello world!
  *
  */
-public class NotesApp
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class NotesApp extends JFrame {
+
+    // text pane
+    private JTextPane textPane;
+
+    // buttons
+    private JButton saveButton;
+    private JButton saveToFileButton;
+    private JButton loadFromFileButton;
+    private JButton deleteButton;
+
+    // my classes - private access modifiers
+    private Note currentNote;
+    private NoteFileManager noteFileManager;
+    private final NodeModifier noteModifier;
+
+    // instances of my Note, NoteFileManager and DatabaseManager classes
+
+
+
+
+    
 }
